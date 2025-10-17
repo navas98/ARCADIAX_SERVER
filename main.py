0c51f719af8bbb4289eb426f6c1271a73127be21
Javier Navas <j.navasdam@gmail.com>
2025-10-17 12:44:03 +0200

    Creacion de la base de datos

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from database.db import db
 app=FastAPI()
 
 #Crear la aplicacion de fastAPI
@@ -7,6 +8,15 @@ app=FastAPI(
     description="API para gestionar videojuegos, peliculas y dispositivos",
     version="1.0.0"
 )
+
+@app.on_event("startup")
+async def check():
+    try:
+        await db.command("ping")
+        print("Mongo db conectada")
+    except Exception as e:
+        print("Mongo db no conectado",e)
+
 @app.get("/ping")
 def ping():
     return{"mensaje":"pong"}
